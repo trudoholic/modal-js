7e4dc5a384c0d28ec1ca2b773522563361de7bb1
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-01-30 23:31:30 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Modal from "./Modal";
+import useModal from './useModal';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {isShowing, toggle} = useModal();
+    return (
+        <div className="App">
+            <button className="button-default" onClick={toggle}>Show Modal</button>
+            <Modal
+                isShowing={isShowing}
+                hide={toggle}
+            />
+        </div>
+    );
 }
 
 export default App;
